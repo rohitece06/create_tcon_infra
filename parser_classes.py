df49b76bc785bb4cb0648e2df37f12e8ce6d5371
RK <rohitkumar.ece06@gmail.com>
2018-09-21 23:09:38 -0700

    Formatting changes

--- parser_classes.py
+++ parser_classes.py
@@ -1,15 +1,15 @@
-from typing import Tuple
-
 class parser:
   def __init__(self, glob, globtype):
     self.decl_block = globtype
-    self.decl_start = ["is"] if globtype in ["entity", "component", "package"] else \
+    self.decl_start = ["is"] if globtype in ["entity", "component", "package"] \
+                      else \
                       ["("] if globtype in ["generic", "port"] else ""
-    self.decl_end = ["end"] if globtype in ["entity", "component", "package"] else \
+    self.decl_end = ["end"] if globtype in ["entity", "component", "package"] \
+                    else \
                     ["]"]  if globtype in ["generic", "port"] else ""
-    self.glob = self.get_glob(glob)
+    self.block = self.get_glob(glob)
 
-  def get_glob(self, glob: str) -> str:
+  def get_glob(self, glob):
     glob_data = ""
     return glob_data
 
@@ -22,17 +22,17 @@ class Ports_generics:
   def __init__(self, glob, globtype):
     self.name, self.typevalues = self.get_entries(glob, globtype)
   
-  def get_entries(self, glob: str, globtype: str) -> Tuple:
+  def get_entries(self, glob, globtype):
     name = ""
     typevalues = Typevalue(glob)
     return name, typevalues
 
 
 class Typevalue:
-  def __init__(self, glob: str) -> None:
+  def __init__(self, glob):
     self.datatype, self.range, self.value, self.direc = self.get_typevalues(glob)
 
-  def get_typevalues(self, glob: str) -> Tuple:
+  def get_typevalues(self, glob):
     datatype = ""
     range = ""
     value = ""
