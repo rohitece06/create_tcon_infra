ed25e0832fe4a86cf7794affa586b2b29bcad7e9
RK <rohitkumar.ece06@gmail.com>
2018-09-27 15:20:23 -0700

    Added files

---
+++ create_tcon_infra.py
@@ -0,0 +1,26 @@
+import re
+import parser_classes as PARSER
+
+if __name__ == "__main__":
+    lines_without_comments =""
+    with open("debounce.vhdl", "r") as f:
+        for line in f.readlines():
+            lines_without_comments += (line.strip("\n")).split("--")[0]
+
+    filestring = re.sub(r'\s+', ' ', lines_without_comments)
+
+
+    # print(filestring)
+    uutname = "debounce"
+    entity_glob = PARSER.ParserType("entity", filestring, uutname).string
+    # print(entity.__dict__)   
+    # print(entity_glob)
+    ports_parser = PARSER.ParserType("port", entity_glob)
+    generics_parser = PARSER.ParserType("generic", entity_glob)
+    entity_inst = PARSER.Entity(ports_parser, generics_parser)
+    with open("filestring.log", "w") as f:
+        for generic in entity_inst.generics:
+            f.write(str(generic)+"\n")        
+        for port in entity_inst.ports:
+            f.write(str(port)+"\n")
+    
